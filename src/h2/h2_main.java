eb7b8886b694b7185843b3f188cb798712acff15
Maxi <maximilian.s107@evt.schule.koeln>
2025-11-06 19:19:44 +0100

    Update h2_main.java

--- src/h2/h2_main.java
+++ src/h2/h2_main.java
@@ -5,7 +5,8 @@ package h2;
 public class h2_main {
     public static void main(String[] args){
         int jahr=2025;
-        boolean schalt=false;
+        boolean schalt;
+        schalt=false;
 
         if (jahr%100==0){
             if (jahr%400==0){
