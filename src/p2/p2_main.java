c78b132eab602fedba530570a71a8282c5253cc9
Maxi <maximilian.s107@evt.schule.koeln>
2025-11-06 17:08:11 +0100

    changed pos of d
    Reintroduced the print statement for variables a, b, and c.

--- src/p2/p2_main.java
+++ src/p2/p2_main.java
@@ -6,10 +6,10 @@ public class p2_main{
     public static void main(String[] args){
         int a=1,b=2,c=3;
         boolean E1=(a==b),E2=(a==b || a==c),E3=((a+b)>=c),E4=((a+b+c)>=100);
-
-        System.out.println("a: "+a+", b: "+b+", c: "+c);
-
         byte d=0;
+        
+        System.out.println("a: "+a+", b: "+b+", c: "+c);
+        
         if (E1){
             d+=1;
         }
