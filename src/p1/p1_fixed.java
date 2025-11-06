32a9db08809347dd45bbc5cf15f9cbf0563321a1
Maxi <maximilian.s107@evt.schule.koeln>
2025-11-06 19:36:14 +0100

    Rename class from p1_main to p1_fixed

--- src/p1/p1_fixed.java
+++ src/p1/p1_fixed.java
@@ -2,7 +2,7 @@
 
 package p1;
 
-public class p1_main{
+public class p1_fixed{
     public static void main(String[] args){
         byte gewuerfelt=1;
         System.out.println("gewürfelt: "+gewuerfelt);
